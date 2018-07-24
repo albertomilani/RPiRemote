43cdc2e6690bd99a230a8aac7e04cdea9fb53429
milhouse <milani.alberto@gmail.com>
2018-07-24 19:21:19 +0200

    ZWO ASI capture

--- client.py
+++ client.py
@@ -4,28 +4,46 @@ import socket
 import sys
 import threading
 import time
+import numpy as np
 
-HOST_ADDRESS = 'localhost'
+HOST_ADDRESS = '192.168.10.44'
 HOST_PORT = 10000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def sendCaptureParams():
     print('Send params')
+    return
     while True:
         sock.sendall('cazzolo')
         time.sleep(0.02)
 
-def receiveImage():
+def receiveImage(connection):
     print('Receive image')
-    while True:
-        data = sock.recv(1024)
-        print data
+    arr = b''
+    count = 0
+    max_time = 5 # seconds
+    time_start = time.time()
+    #while len(arr) < 1228800:
+    while len(arr) < 307200:
+        if (time.time() - time_start) > max_time:
+            print('Timeout')
+            break;
+        data = connection.recv(4096)
+        if data:
+            count = count + 1
+            arr += data
+    time_end = time.time()
+    total_time = time_end - time_start
+    print('Total time:', total_time)
+    #array = np.frombuffer(arr, dtype=np.dtype(np.uint8)).reshape((960,1280))
+    array = np.frombuffer(arr, dtype=np.dtype(np.uint8)).reshape((480,640))
+    print array
 
 sock.connect((HOST_ADDRESS, HOST_PORT))
 
 th1 = threading.Thread( target=sendCaptureParams, args=() )
-th2 = threading.Thread( target=receiveImage, args=() )
+th2 = threading.Thread( target=receiveImage, args=(sock,) )
 
 th1.start()
 th2.start()
--- server.py
+++ server.py
@@ -6,8 +6,9 @@ import sys
 import os
 import threading
 import time
+import zwoasi as asi
 
-HOST_ADDRESS = 'localhost'
+HOST_ADDRESS = '0.0.0.0'
 HOST_PORT = 10000
 
 # CCD global variables
@@ -20,52 +21,89 @@ class CameraHandler():
         self.camera = camera
 
     def Capture(self, exposure_time, gain_value):
-        data = numpy.array(numpy.random.random((960,1280))*100,dtype=int)
-        return data
+        # example data
+        #data = numpy.array(numpy.random.random((960,1280))*100,dtype=numpy.uint8)
+        #return data
 
-    def TestMsg(self):
-        return 't0p4'
+        # Use minimum USB bandwidth permitted
+        self.camera.set_control_value(asi.ASI_BANDWIDTHOVERLOAD, self.camera.get_controls()['BandWidth']['MinValue'])
+
+        self.camera.set_control_value(asi.ASI_GAIN, gain_value) # range 0-300
+        self.camera.set_control_value(asi.ASI_EXPOSURE, exposure_time) # microsecond
+        self.camera.set_control_value(asi.ASI_WB_B, 99)
+        self.camera.set_control_value(asi.ASI_WB_R, 75)
+        self.camera.set_control_value(asi.ASI_GAMMA, 50)
+        self.camera.set_control_value(asi.ASI_BRIGHTNESS, 50)
+        self.camera.set_control_value(asi.ASI_FLIP, 0)
+        self.camera.set_roi(bins=2)
+        self.camera.set_image_type(asi.ASI_IMG_RAW8)
+
+        try:
+            # Force any single exposure to be halted
+            self.camera.stop_video_capture()
+            self.camera.stop_exposure()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            pass
+
+        return self.camera.capture()
+
 
 def readCaptureParams():
 
     global exp_time
     global gain
-
-    print('Start reading')
-
+    return
     while True:
         data = conn.recv(1024)
         print(data)
 
 
-def captureAndSend():
+def captureAndSend(camera):
 
-    ccd = CameraHandler(None)
+    ccd = CameraHandler(camera)
 
-    print('Start writing')
+    image = ccd.Capture(exp_time, gain);
+    print image
+    im_bytes = image.tobytes()
+
+    chunk_size = 1024
+    msg_len = len(im_bytes)
+    print msg_len
+
+    for i in xrange((msg_len/chunk_size)+1):
+        start = i * chunk_size
+        end = start + chunk_size
+        if end > msg_len:
+            end = msg_len
+        conn.sendall(im_bytes[start:end])
+
+if __name__ == "__main__":
+
+    libasi_path = '/opt/ASI_SDK/lib/armv7/libASICamera2.so'
+    asi.init(libasi_path)
+
+    camera0 = asi.Camera(0)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    #sock.setblocking(0)
+    sock.bind((HOST_ADDRESS, HOST_PORT))
+    sock.listen(2)
 
     while True:
-        #image = ccd.Capture(exp_time, gain)
-        msg = ccd.TestMsg()
-        conn.sendall(msg)
-        time.sleep(0.0333)
+        conn, addr = sock.accept()
+        thread1 = threading.Thread( target=readCaptureParams, args=() )
+        thread2 = threading.Thread( target=captureAndSend, args=(camera0,) )
+
+        thread1.start()
+        thread2.start()
+
+        thread1.join()
+        thread2.join()
+        break
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((HOST_ADDRESS, HOST_PORT))
-sock.listen(2)
-
-while True:
-    conn, addr = sock.accept()
-    thread1 = threading.Thread( target=readCaptureParams, args=() )
-    thread2 = threading.Thread( target=captureAndSend, args=() )
-
-    thread1.start()
-    thread2.start()
-
-    thread1.join()
-    thread2.join()
-
-
-sock.close()
+    sock.close()
 
